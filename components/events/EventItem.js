6f1aa557ff11c83e4da96361103b1e07ef2e031a
KKsimform <kishan.k@simformsolutions.com>
2022-09-07 15:02:35 +0530

    Add style to component

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import classes from './event-item.module.css';
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const formattedDate = new Date(date).toLocaleDateString('en-US', {
@@ -10,21 +10,19 @@ const EventItem = (props) => {
   const formattedAddress = location.replace(', ', '\n');
   const exploreLink = `events/${id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <div>
-          <div>
-            <h2>{title}</h2>
-          </div>
-          <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
             <time>{formattedDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,11 +1,13 @@
 import EventItem from './EventItem';
+import classes from './event-list.module.css';
 
 const EventList = (props) => {
   const { items } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((event) => (
         <EventItem
+          key={event.id}
           id={event.id}
           title={event.title}
           date={event.date}
