72665cbf0552550e322706f0a4c70efb61ca3995
KKsimform <kishan.k@simformsolutions.com>
2022-09-08 11:09:45 +0530

    feature: Event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,27 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import { getEventById } from '../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+
+  if (!event) return <p>No Event found!</p>;
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
   );
 };
 export default EventDetailPage;
