c2c3146ca8a3fe69b4611d35e06006486352a6fe
KKsimform <kishan.k@simformsolutions.com>
2022-09-14 17:36:49 +0530

    add Head tag

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import EventList from '../components/events/EventList';
 import { getFeaturedEvents } from './../helpers/api-utils';
+import Head from 'next/head';
 
 const HomePage = ({ featuredEvents }) => {
   return (
     <div>
+      <Head>
+        <title>Next.JS Events</title>
+        <meta name="description" content="Find a lot of events here that help you to evolve....."/>
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
